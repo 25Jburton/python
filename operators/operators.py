87f969293a985bf0824b0617132cc9144e4b3873
25Jburton <25jburton@gmail.com>
2025-04-15 13:54:45 -0400

    operator section complete

---
+++ operators/operators.py
@@ -0,0 +1,29 @@
+# +	Addition
+# -	Subtraction
+# *	Multiplication
+# /	Division (Floating)
+# //	Floor Division
+# %	Modulus (Remainder)
+# **	Exponentiation
+
+# +=	Add and Assign
+# -=	Subtract and Assign
+# *=	Multiply and Assign
+# /=	Divide and Assign
+# //=	Floor Divide and Assign
+# %=	Modulus and Assign
+# **=	Exponent and Assign
+
+# < Less than
+# <= Less than or equal
+# > Greater than
+# >= Greater than or equal
+# Equal ==
+# Not equal !=
+
+# Logical operators (and, or, not) -  highest precedence in Python is 'not' operator
+price = 9.99
+output_and = price > 9 and price < 10
+output_or = price > 10 or price < 20
+output_not = not (price > 5 and price < 10)
+
